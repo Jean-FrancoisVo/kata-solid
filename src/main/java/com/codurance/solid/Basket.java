5f91b0971649e90e45cb5bd45e1906f69c3625a0
Jean-François VO <jean-francois-externe.vo@edf.fr>
2018-06-06 14:29:40 +0200

    Add map for book type per price or per discount and extracted method

--- src/main/java/com/codurance/solid/Basket.java
+++ src/main/java/com/codurance/solid/Basket.java
@@ -1,78 +1,89 @@
 package com.codurance.solid;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static com.codurance.solid.BookType.*;
 import static com.codurance.solid.BookType.IT;
 import static com.codurance.solid.BookType.TRAVEL;
 import static java.lang.Math.round;
 import static java.util.Collections.unmodifiableList;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Basket {
 
-	private List<Book> books = new ArrayList<Book>();
+    private List<Book> books = new ArrayList<>();
 
-	public void add(Book book) {
-		books.add(book);
-	}
+    public void add(Book book) {
+        books.add(book);
+    }
 
-	public List<Book> books() {
-		return unmodifiableList(books);
-	}
+    List<Book> books() {
+        return unmodifiableList(books);
+    }
 
 
-	public double priceWithDiscount() {
-		double it_books_discount = 0;
-		double travel_books_discount = 0;
-		
-		double number_of_it_books = 0;
-		double number_of_travel_books = 0;
-		
-		double total_price_for_it_books = 0;
-		double total_price_for_travel_books = 0;
-		double total_price_for_other_books = 0;
-		
-		for (Book book : this.books) {
-			if (IT.equals(book.type())) {
-				number_of_it_books += 1;
-				total_price_for_it_books += book.price();
-			} else if(TRAVEL.equals(book.type())){
-				number_of_travel_books += 1;
-				total_price_for_travel_books += book.price();
-			} else {
-				total_price_for_other_books += book.price();
-			}
-		}
-		
-		if (number_of_it_books > 2) {
-			it_books_discount = 0.7; // 30% discount when buying more than 2 IT books
-		} else if (number_of_it_books > 0) {
-			it_books_discount = 0.9; // 10% discount when buying up to 2 IT books
-		}
-		
-		if (number_of_travel_books > 3) {
-			travel_books_discount = 0.6; // 40% discount when buying more than 3 travel books
-		}
-		
-		if (travel_books_discount > 0) {
-			total_price_for_travel_books *= travel_books_discount;
-		}
-		
-		return toDecimal((
-				total_price_for_it_books * it_books_discount)
-				+ total_price_for_travel_books
-				+ total_price_for_other_books);
-	}
+    double priceWithDiscount() {
 
-	public double fullPrice() {
-		double price = 0;
-		for (Book book : books) {
-			price += book.price();
-		}
-		return toDecimal(price);
-	}
+        double number_of_it_books = 0;
+        double number_of_travel_books = 0;
 
-	private double toDecimal(double number) {
-		return round(number * 100) / 100.0;
-	}
+        Map<BookType, Double> discountPerBookType = new HashMap<>();
+        for (BookType bookType: BookType.values()) {
+            discountPerBookType.put(bookType, 1.);
+        }
+        for (Book book: this.books) {
+            if (IT.equals(book.type())) {
+                number_of_it_books += 1;
+            } else if (TRAVEL.equals(book.type())) {
+                number_of_travel_books += 1;
+            }
+        }
+        if (number_of_it_books > 2) {
+            discountPerBookType.put(IT, 0.7);
+        } else if (number_of_it_books > 0) {
+            discountPerBookType.put(IT, 0.9);
+        }
+
+        if (number_of_travel_books > 3) {
+            discountPerBookType.put(TRAVEL, 0.6);
+        }
+
+
+        Map<BookType, Double> pricePerBookType = getPricePerBookType();
+
+        return computeTotalPriceWithDiscount(discountPerBookType, pricePerBookType);
+    }
+
+    private double computeTotalPriceWithDiscount(Map<BookType, Double> discountPerBookType, Map<BookType, Double> pricePerBookType) {
+        double totalPrice = 0d;
+        for (BookType bookType: BookType.values()) {
+            totalPrice += pricePerBookType.get(bookType) * discountPerBookType.get(bookType);
+        }
+        return toDecimal(totalPrice);
+    }
+
+    private Map<BookType, Double> getPricePerBookType() {
+        Map<BookType, Double> pricePerBookType = new HashMap<>();
+        for (BookType bookType : values()) {
+            pricePerBookType.put(bookType, 0.);
+        }
+        for (Book book : this.books) {
+            pricePerBookType.put(book.type(), pricePerBookType.get(book.type()) + book.price());
+        }
+        return pricePerBookType;
+    }
+
+    double fullPrice() {
+        double price = 0;
+        for (Book book : books) {
+            price += book.price();
+        }
+        return toDecimal(price);
+    }
+
+    private double toDecimal(double number) {
+        return round(number * 100) / 100.0;
+    }
 }
